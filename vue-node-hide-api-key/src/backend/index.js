ea81c71704f03656652c06b7d4a4cce001155431
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 12:05:15 +0300

    Rokas

--- vue-node-hide-api-key/src/backend/index.js
+++ vue-node-hide-api-key/src/backend/index.js
@@ -12,6 +12,7 @@ const apiUrl = 'http://api.openweathermap.org/data/2.5/weather'
 const apiKey = '7072edc8ba7df4103c3a2efc6061be00'
 // creating endpoint, to return something
 app.get('/', (request, response) => {
+    response.set("Access-Control-Allow-Origin", "*")
     // console.log(request.query)
     // this makes the new object from request query, what comes from front end
     // also adds the api key from backend
@@ -21,8 +22,8 @@ app.get('/', (request, response) => {
     let queryString = qs.stringify(query)
     // console.log(queryString)
     // we make the api request to the weathere site with the following strings
-    axios(`${apiUrl}?${queryString}`).then(response => {
-        console.log(response);
+    axios(`${apiUrl}?${queryString}`).then(res => {
+        response.send(res.data);
     })
 })
 
