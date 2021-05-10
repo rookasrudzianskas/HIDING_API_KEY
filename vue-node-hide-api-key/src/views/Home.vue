682b8b89269e6bed155279797756409e266d5cd8
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 11:18:00 +0300

    Rokas

--- vue-node-hide-api-key/src/views/Home.vue
+++ vue-node-hide-api-key/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Weather App</h1>
+    <div class="enter-city">
+      <input type="text" placeholder="Enter a city">
+    </div>
+    <div class="weather">
+      <h1>6&deg;</h1>
+      <h2>Cloudy</h2>
+      <h3>Loads of clouds</h3>
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 }
 </script>
+
+<style lang="sass">
+.enter-city
+  input
+    font-size: 40px
+    outline: none
+    border-radius: 999px
+    border: 1px solid lightgray
+.weather
+  margin-top: 10px
+  h1
+    font-size: 80px
+  h1, h2, h3
+   margin: 0
+</style>
+
