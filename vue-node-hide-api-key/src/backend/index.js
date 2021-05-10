0b2170ba27006517936b9bc96d700054375fc7b4
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 12:03:05 +0300

    Rokas

--- vue-node-hide-api-key/src/backend/index.js
+++ vue-node-hide-api-key/src/backend/index.js
@@ -1,18 +1,29 @@
 // importing express framework
+var qs = require('qs');
+
 const express = require('express')
+const axios = require('axios')
 // setting the app
 const app = express()
 // and opening it on this port
 const port = 3000
 
-// const apiUrl = 'http://api.openweathermap.org/data/2.5/weather'
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather'
 const apiKey = '7072edc8ba7df4103c3a2efc6061be00'
 // creating endpoint, to return something
 app.get('/', (request, response) => {
     // console.log(request.query)
+    // this makes the new object from request query, what comes from front end
+    // also adds the api key from backend
+    // and then with qs stringify we turn the object into one bis string to make the request
     let query = request.query
     query.appid = apiKey
-    console.log(query)
+    let queryString = qs.stringify(query)
+    // console.log(queryString)
+    // we make the api request to the weathere site with the following strings
+    axios(`${apiUrl}?${queryString}`).then(response => {
+        console.log(response);
+    })
 })
 
 // launching app on specified port
